e6826e37516a57269b748ca49f7a613d60dee07f
klara_cp <klara.cedeno@toreiconsulting.com>
2022-03-31 16:00:44 -0400

    Upload files

---
+++ cedeno.klara/src/test/java/be/adidas/cedeno/klara/AppTest.java
@@ -0,0 +1,20 @@
+package be.adidas.cedeno.klara;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
